c4c83daef4728dcfa3d369fe0d813fd2915698e8
Ardo Dlamini <ardodlamini@gmail.com>
2021-06-14 12:10:27 +0200

    All atoms are bonding.

--- src/Carbon.java
+++ src/Carbon.java
@@ -1,4 +1,5 @@
 package molecule;
+import java.util.concurrent.Semaphore;
 
 public class Carbon extends Thread {
 	
@@ -6,6 +7,7 @@ public class Carbon extends Thread {
 	private int id;
 	private Propane sharedPropane;
 	public static BarrierReusable barrier = new BarrierReusable(3); // all atoms must be at the barrier //3 carbon atoms in Propane
+	public static Semaphore sem = new Semaphore(3);
 	
 	public Carbon(Propane propane_obj) {
 		 
@@ -17,16 +19,22 @@ public class Carbon extends Thread {
 	public void run() {
 	    try {	 
 	    	 // you will need to fix below
-	    	
+	    	System.out.println("Carbon "+ this.id +" Present");
+	    	sem.acquire();
 	    	barrier.phase1(); //Only 3 carbons at a time are allowed through.
+	    	
 	    	System.out.println(this.id+" Carbon through.");
-	    	sharedPropane.barrier.b_wait();
+	    	
+	    	//sharedPropane.barrier.b_wait();
+	    	
 	    	System.out.println("---Group ready for bonding---");
 	    	sharedPropane.addCarbon();
 	    	sharedPropane.bond("C"+ this.id);  //bond
-	    	sharedPropane.removeCarbon(1);
-	    	barrier.phase2();
-	    	 
+	    	
+	    	System.out.println(barrier.count);
+	    	barrier.phase2(); //The semaphore and Barrier are necessary as we want to make sure all 3 threads are done first and the count is 0 before any other thread comes in.
+	    	sem.release();
+	    	System.out.println("Carbon Exit!");
 	    }
 	    catch (InterruptedException ex) { /* not handling this  */}
 	   // System.out.println(" ");
--- src/Hydrogen.java
+++ src/Hydrogen.java
@@ -1,4 +1,5 @@
 package molecule;
+import java.util.concurrent.Semaphore;
 
 public class Hydrogen extends Thread {
 
@@ -6,6 +7,7 @@ public class Hydrogen extends Thread {
 	private int id;
 	private Propane sharedPropane;
 	public static BarrierReusable barrier =  new BarrierReusable(8); //8 hydrogen atoms in Propane.; // all atoms must be at the barrier
+	public static Semaphore sem = new Semaphore(8);
 	
 	public Hydrogen(Propane propane_obj) {
 		Hydrogen.hydrogenCounter+=1;
@@ -17,13 +19,21 @@ public class Hydrogen extends Thread {
 	public void run() {
 	    try {
 	    	 // you will need to fix below
-	    	
+	    	System.out.println("Hydrogen "+ this.id +" Present");
+	    	sem.acquire();
 	    	barrier.phase1(); //Only 8 hydrogens at a time are allowed through.
+	    	
 	    	System.out.println(this.id + " hydrogen through.");
-	    	sharedPropane.barrier.b_wait();
+	    	
+	    	//sharedPropane.barrier.b_wait();
+	    	
 	    	sharedPropane.addHydrogen();			 
 	    	sharedPropane.bond("H"+ this.id);
+	    	
+	    	System.out.println(barrier.count);
 	    	barrier.phase2();
+	    	sem.release();
+	    	System.out.println("Carbon Exit!");
 	    }
 	   catch (InterruptedException ex) { /* not handling this  */}
 	    //System.out.println(" ");
